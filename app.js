37423bfc9b47f267bdd6db63e99fc757fcbe168d
HannesNyfelt <90187682+HannesNyfelt@users.noreply.github.com>
2022-01-12 14:01:47 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    document.getElementById("real-dropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('.button')) {
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
